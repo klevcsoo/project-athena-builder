539d8c102f13cff0d1c69aa549d154068200ccab
Keve Lajsz <kevelajsz@icloud.com>
2023-01-20 23:30:01 +0100

    Add BasicButton.tsx

---
+++ src/components/ui/BasicButton.tsx
@@ -0,0 +1,21 @@
+import {cnx} from "../../core/util";
+
+export function BasicButton(props: {
+    text: string
+    onClick(): void
+    disabled?: boolean
+    warning?: boolean
+}) {
+    return (
+        <button type={"button"} disabled={props.disabled} className={cnx(
+            "w-full", "max-w-xs", "h-12",
+            props.warning ? "bg-red-600" : "bg-white",
+            "rounded-md",
+            props.warning ? "hover:bg-red-700" : "hover:bg-neutral-200"
+        )} onClick={props.onClick}>
+            <b className={props.warning ? "text-white" : "text-black"}>
+                {props.text}
+            </b>
+        </button>
+    );
+}
