35066af5714431be36ae6724939fe526465ac994
Keve Lajsz <kevelajsz@icloud.com>
2023-01-20 16:59:43 +0100

    Add tool keyboard shortcuts

--- src/hooks/useActiveTool.ts
+++ src/hooks/useActiveTool.ts
@@ -2,6 +2,21 @@ import {ToolboxTool} from "../lib/ToolboxTool";
 import {useCallback, useEffect, useState} from "react";
 
 const LS_KEY = "toolbox.active_tool";
+const toolboxKeyMap: Map<string, ToolboxTool> = new Map([
+    ["KeyV", "pointer"],
+    ["KeyP", "platform"]
+]);
+
+// Initialize toolbox shortcuts
+// Refuses to work if I don't put the in an
+// anonymous function for some reason
+(() => {
+    window.addEventListener("keypress", ({code}) => {
+        if (toolboxKeyMap.has(code)) {
+            localStorage.setItem(LS_KEY, toolboxKeyMap.get(code)!);
+        }
+    });
+})();
 
 export function useActiveTool(): [ToolboxTool, (tool: ToolboxTool) => void] {
     const [tool, setTool] = useState<ToolboxTool>(localStorage.getItem(LS_KEY) as any);
